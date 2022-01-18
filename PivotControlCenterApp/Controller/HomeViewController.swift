648125e0fb0f145515f4f08c3f6ddbde72e973fe
deLore051 <stefan.dojcinovic051@gmail.com>
2022-01-18 11:49:05 +0100

    First commit

---
+++ PivotControlCenterApp/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PivotControlCenterApp
+//
+//  Created by Ingel Agro on 18.1.22..
+//
+
+import UIKit
+
+class 	ller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
